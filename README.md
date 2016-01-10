d8db524210c5d9f1b6bb991ba1f6706a8f964c5c
Bonno <github@bonno.bo-ma.nl>
2016-01-10 14:20:49 +0100

    +: Added license file header and created usage documentation

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# ing2genericcsv
+# ing2generic
+
+ This script converts the ing <www.ing.nl> csv files into more generic csv files which can be imported by FireflyIII <https://github.com/JC5/firefly-iii>
+ 
+ Usage
+-----
+::
+
+    usage: ing2generic [-h] [-o, --outfile OUTFILE] [-d, --directory DIR] [-c, --convert] csvfile
+
+    This program converts ING (www.ing.nl) CSV files to generic csv files. The default output filename is the input filename.
+
+    positional arguments:
+      csvfile                      A csvfile to process
+
+    optional arguments:
+      -h, --help                 show this help message and exit
+      -o, --outfile OUTFILE Output filename
+      -d, --directory DIR   Directory to store output, default is ./converted
+      -c, --convert            Convert decimal separator to dots (.), default is false
+
+
+Output
+------
+#. A generic csv file converted from the csv file (default in the folder ./converted)
--- ing2generic.py
+++ ing2generic.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# This script converts the ing <www.ing.nl> csv files into more generic csv files which can be imported by FireflyIII <https://github.com/JC5/firefly-iii>
+# Copyright (C) 2016 Bonno Nachtegaal-Karels
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import csv
 import argparse
@@ -65,8 +82,7 @@ def OpposingName(name):
 
 """ First parse the command line arguments. """
 parser = argparse.ArgumentParser(prog = 'ing2generic', description= """
-                                 This program converts ING (www.ing.nl) CSV files to OFX format.
-                                 The default outfile filename is the input filename.
+                                 This program converts ING (www.ing.nl) CSV files to generic csv files. The default output filename is the input filename.
                                  """)
 parser.add_argument('csvfile', help='A csvfile to process')
 parser.add_argument('-o, --outfile', dest = 'outfile', 
